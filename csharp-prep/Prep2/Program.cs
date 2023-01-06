3b1ea9bcaa7b37e1047c16e58e0aa076aa08eb04
Sam Anderson <and16096@byui.edu>
2023-01-05 22:06:19 -0700

    Compleated practice

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string numberString = Console.ReadLine();
+        int number = int.Parse(numberString);
+
+
+
+
+
+        if (number>90)
+        {
+            Console.WriteLine($"You passed with an A, {number}%");
+        }
+        else if (number>80)
+        {
+            Console.WriteLine($"You passed with a B, {number}%");
+        }
+        else if (number>70)
+        {
+            Console.WriteLine($"You passed with a C, {number}%");
+        }
+        else if (number>60)
+        {
+            Console.WriteLine($"You failed with a D, {number}%");
+        }
+        else if (number>50)
+        {
+            Console.WriteLine($"You failed with a F, {number}%");
+        }
+        else if (number>40)
+        {
+            Console.WriteLine($"You failed with a F, {number}%");
+        }
+        else if (number>30)
+        {
+            Console.WriteLine($"You failed with a F, {number}%");
+        }
+        else if (number>20)
+        {
+            Console.WriteLine($"You failed with a F, {number}%");
+        }
+        else if (number>10)
+        {
+            Console.WriteLine($"You failed with a F, {number}%");
+        }
+        else 
+        {
+            Console.WriteLine($"You failed with a F, {number}%");
+        }
+        
     }
 }
