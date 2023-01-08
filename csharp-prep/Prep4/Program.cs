b481bbec65494fc666bd93627711cca4908db603
Sam Anderson <and16096@byui.edu>
2023-01-07 23:20:45 -0700

    Work in progress

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        
+        List<int> numList = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+
+        int userNum = -1;
+        while (userNum != 0)
+        {
+        
+            Console.WriteLine("Enter number: ");
+            string userStr = Console.ReadLine();
+            userNum = int.Parse(userStr);
+
+            if (userNum != 0)
+            {
+                numList.Add(userNum);
+            }
+
+
+
+
+        }
+
+        int sum = 0;
+        foreach (int number in numList)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is {sum}");
+
+
+
+
+
+
     }
 }
