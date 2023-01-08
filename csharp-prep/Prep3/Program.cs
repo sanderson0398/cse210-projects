a72dda02dc3f36b59fe203add3205cc690c4efd5
Sam Anderson <and16096@byui.edu>
2023-01-07 20:44:08 -0700

    Compleated Streach goals

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,15 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
+        string input = "yes";
+        do
+        {
+        
         Random randomGenerator = new Random();
         int number = randomGenerator.Next(1, 100);
         
 
         string guess = "";
         int nGuess = 0;
-        
+        int count = 0;
+
         while (nGuess != number )
         {
+        count = count + 1;
         Console.Write("What is the magic number? ");
         Console.Write("What is your guess? ");
         guess = Console.ReadLine();
@@ -30,8 +36,12 @@ class Program
         else
         {
             Console.WriteLine("You guessed it!");
+            Console.WriteLine($"It took {count} tries");
         }
+        }   Console.Write("Do you want to continue? ");
+            input = Console.ReadLine();
         }
+            while (input == "yes");
         
 
 
