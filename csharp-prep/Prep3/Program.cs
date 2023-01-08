3d5105018aabe7e7921d657e7978f7b0b0a8e72d
Sam Anderson <and16096@byui.edu>
2023-01-07 20:04:32 -0700

    Counts each time the loop runs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        
+
+        string guess = "";
+        int nGuess = 0;
+        
+        while (nGuess != number )
+        {
+        Console.Write("What is the magic number? ");
+        Console.Write("What is your guess? ");
+        guess = Console.ReadLine();
+        nGuess = int.Parse(guess);
+        
+        if (nGuess < number)
+        {
+            Console.WriteLine("Higher. Guess again" );            
+        }
+        else if (nGuess > number)
+        {
+            Console.WriteLine("Lower. Guess again");
+            
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
+        }
+        
+
+
+
     }
 }
