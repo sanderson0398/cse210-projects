34004956db3a7d4ccab67f149a3ea2804c403aab
Sam Anderson <and16096@byui.edu>
2023-01-11 15:39:30 -0700

    Works execpt ofr DisplayResult()

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        
+        static void DisplayMessage()
+        {
+                Console.WriteLine("Welcome to the Program! ");
+        }
+
+        static void  PromptUserName()
+        {
+                Console.Write("Type User Name: ");
+                string name = Console.ReadLine();
+        }
+
+        static void PromptUserNumber()
+        {
+                Console.Write("Type User Number: ");
+                Console.ReadLine();
+        } 
+
+        static void SquareNumber()
+        {
+                
+                Console.Write("What is your favorite number?: ");
+                string favNum = Console.ReadLine();
+                int intFavNum = int.Parse(favNum);
+                int sqNum = intFavNum^2;         
+   
+        }
+
+        /*static void DisplayResult(string name,int sqNum)
+        {
+                Console.WriteLine($"Hello {name}, your favorite num Squred is {sqNum}");
+        }
+        */
+
+        static void DisplayResult(string name, int sqNum)
+            {
+                        Console.WriteLine($"Hello {name}, the square of your number is {sqNum}");
+            }
+            
+        DisplayMessage();
+        PromptUserName();
+        PromptUserNumber();
+        SquareNumber();
+        DisplayResult(name, sqNum);              
+
+
+
+
+
+
+
     }
 }
